0b02b7c44209676bab6253cc6f0904e0f3eeccc3
Scott Morgan <scott.morgan3142@gmail.com>
2022-03-22 09:46:39 +0000

    Version 1.1 - adds some optimisation - breaks out room functionality into unit functions

--- src/main.py
+++ src/main.py
@@ -15,44 +15,32 @@ def main():
       direction = input("Which direction do you want to go? Please choose from North, South, East or West.")    
 
       if direction == "North":
-        print(f"You entered the North Room.")
-        puzzle_guess = input("What is 2 + 2? ")
-        if puzzle_guess == "4":
-          print(f"Correct. Key 1 collected.")
-          backpack.append(f"Key 1")
-        else:
-          lives -= 1
-          print(f"Incorrect. You have {lives} lives remaining.")
+        room_direction = "North"
+        puzzle = "What is 2 + 2? "
+        puzzle_solution = "4"
+        key_number = "1"
+        lives = in_room(backpack,lives,room_direction,puzzle,puzzle_solution,key_number)
 
       elif direction == "South":
-        print(f"You entered the South Room.")
-        puzzle_guess = input("What is 2 + 3? ")
-        if puzzle_guess == "5":
-          print(f"Correct. Key 2 collected.")
-          backpack.append(f"Key 2")
-        else:
-          lives -= 1
-          print(f"Incorrect. You have {lives} lives remaining.")
+        room_direction = "South"
+        puzzle = "What is 2 + 3? "
+        puzzle_solution = "5"
+        key_number = "2"
+        lives = in_room(backpack,lives,room_direction,puzzle,puzzle_solution,key_number)
 
       elif direction == "East":
-        print(f"You entered the East Room.")
-        puzzle_guess = input("What is 2 + 4? ")
-        if puzzle_guess == "6":
-          print(f"Correct. Key 3 collected.")
-          backpack.append(f"Key 3")
-        else:
-          lives -= 1
-          print(f"Incorrect. You have {lives} lives remaining.")
-
+        room_direction = "East"
+        puzzle = "What is 2 + 4? "
+        puzzle_solution = "6"
+        key_number = "3"
+        lives = in_room(backpack,lives,room_direction,puzzle,puzzle_solution,key_number)
+      
       elif direction == "West": 
-        print(f"You entered the West Room.")
-        puzzle_guess = input("What is 2 + 5? ")
-        if puzzle_guess == "7":
-          print(f"Correct. Key 4 collected.")
-          backpack.append(f"Key 4")
-        else:
-          lives -= 1
-          print(f"Incorrect. You have {lives} lives remaining.")
+        room_direction = "West"
+        puzzle = "What is 2 + 5? "
+        puzzle_solution = "7"
+        key_number = "4"
+        lives = in_room(backpack,lives,room_direction,puzzle,puzzle_solution,key_number)
 
       else:
         print("Room not recognised!")   
